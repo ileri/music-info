03d8b8a4d2be1c6f0e938089a648602a13674b83
celilileri <celil.ileri@bil.omu.edu.tr>
2017-03-16 16:53:20 +0300

    Multiple voice file add info

--- minfo.rb
+++ minfo.rb
@@ -5,7 +5,7 @@ def all_files(directory)
   Dir.glob File.join(directory, '**/*')
 end
 
-def music_info(file_addr, *infos)
+def music_info(file_addr, infos)
   Mp3Info.open file_addr do |file|
     file.tag.title = infos[0] if infos[0]
     file.tag.artist = infos[1] if infos[1]
@@ -14,8 +14,21 @@ def music_info(file_addr, *infos)
   end
 end
 
+def bulk_music_info(file_list, music_infos)
+  if file_list.size == music_infos.lines.count
+    file_list.each_with_index do |file, i|
+      infos = music_infos.lines[i].split('-').each(&:chomp)
+      music_info file, infos
+    end
+  else
+    raise 'Number of files and infos is not equal!'
+  end
+end
+
 def main
-  file_list = all_files '/home/sci/Workspace'
+  file_list = all_files '.'
+  music_infos = File.read 'music_infos.txt'
+  bulk_music_info file_list, music_infos
 end
 
 main if __FILE__ == $PROGRAM_NAME
