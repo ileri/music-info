f5be4841647d0e2019fbca9aca0716aef5f92bb3
celilileri <celil.ileri@bil.omu.edu.tr>
2017-03-17 23:07:18 +0300

    Dosya adını title yapma desteği eklendi

--- minfo.rb
+++ minfo.rb
@@ -5,7 +5,7 @@ def all_files(directory)
   Dir.glob File.join(directory, '**', '*')
 end
 
-def music_info(file_addr, infos)
+def music_info(file_addr, *infos)
   Mp3Info.open file_addr do |file|
     file.tag.title = infos[0] if infos[0]
     file.tag.artist = infos[1] if infos[1]
@@ -14,21 +14,27 @@ def music_info(file_addr, infos)
   end
 end
 
+def filenames_to_tag(file_list)
+  file_list.each do |file|
+    music_info file, File.basename(file, '.*')
+  end
+end
+
 def bulk_music_info(file_list, music_infos)
-  if file_list.size == music_infos.lines.count
-    file_list.each_with_index do |file, i|
-      infos = music_infos.lines[i].split('-').each(&:chomp)
-      music_info file, infos
-    end
-  else
+  unless file_list.size == music_infos.lines.count
     raise 'Number of files and infos is not equal!'
+  end
+  file_list.each_with_index do |file, i|
+    infos = music_infos.lines[i].split('-').each(&:strip!)
+    music_info file, *infos
   end
 end
 
 def main
   file_list = all_files 'musics'
   music_infos = File.read 'music_infos.txt'
-  bulk_music_info file_list, music_infos
+  # bulk_music_info file_list, music_infos
+  # filenames_to_tag file_list 
 end
 
 main if __FILE__ == $PROGRAM_NAME
